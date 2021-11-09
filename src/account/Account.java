cc24e9e9e0ac3ebbc542803ce8408084ad0bfce3
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-09 20:29:09 +0530

    account added

---
+++ src/account/Account.java
@@ -0,0 +1,26 @@
+package account;
+import java.util.Scanner;
+
+public class Account {
+
+    public static void main(String[] args) {
+        AccountTest.debit();
+    }
+}
+
+class AccountTest {
+    static long debitAmount;
+    static long accountBalance = 1000l;
+
+    public static void debit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Debit amount");
+        debitAmount = sc.nextLong();
+        if (debitAmount > accountBalance) {
+            System.out.println("Debit amount exceeded account balance");
+            System.out.println("Account balance is = " + accountBalance);
+        } else
+            System.out.println("Debited amount is " + " " + debitAmount);
+        System.out.println("Account balance is " + (accountBalance - debitAmount));
+    }
+}
